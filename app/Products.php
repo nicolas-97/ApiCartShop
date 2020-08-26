85f8d2068b0696b81a9ce78b32ee1cd3dd3063d2
Nicolas <nicolas.sabogalto@utadeo.edu.co>
2020-08-26 10:10:47 -0500

    Api product

--- app/Products.php
+++ app/Products.php
@@ -6,5 +6,5 @@ use Illuminate\Database\Eloquent\Model;
 
 class Products extends Model
 {
-    protected $filleable = ['name','sku','description'];
+    protected $fillable = ["name","sku","description"];
 }
